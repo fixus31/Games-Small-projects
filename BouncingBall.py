4db9ecaca0178453246ee32d45939a04c7357eb4
George M <69652018+fixus31@users.noreply.github.com>
2022-12-17 18:48:03 -0500

    Update BouncingBall.py

--- BouncingBall.py
+++ BouncingBall.py
@@ -2,6 +2,34 @@ from tkinter import *
 import random
 import time
 
+class Ball:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
+        self.canvas.move(self.id, 240, 100)
+        starts = [-3, -2, -1, 1, 2, 3]
+        # start location is random
+        random.shuffle(starts)
+        self.x = starts[0]
+        self.y = -3
+        #returns current height of canvas
+        self.canvas_height = self.canvas.winfo_height()
+        self.canvas_width = self.canvas.winfo_width()
+
+    def draw(self):
+        self.canvas.move(self.id, self.x, self.y)
+        pos = self.canvas.coords(self.id)
+        if pos[1] <= 0:
+            self.y = 3
+        if pos[3] >= self.canvas_height:
+            self.y = -3
+        if pos[0] <= 0:
+            self.x = 3
+        if pos[2] >= self.canvas_width:
+            self.x = -3
+        
+
+
 tk = Tk()
 tk.title("Game: Bouncing Ball")
 tk.resizable(0, 0)
@@ -9,4 +37,12 @@ tk.wm_attributes("-topmost", 1)
 canvas = Canvas(tk, width=600, height=600, bd=0, highlightthickness=0)
 canvas.pack()
 tk.update()
-tk.mainloop()
+
+ball = Ball(canvas, 'red')
+
+while True:
+    ball.draw()
+    tk.update_idletasks()
+    tk.update()
+    time.sleep(0.01)
+#tk.mainloop()
