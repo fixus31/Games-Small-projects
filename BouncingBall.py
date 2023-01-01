b6d9670dd7878a5ae109f1102d87bb94c81b05e9
George M <69652018+fixus31@users.noreply.github.com>
2023-01-01 18:59:30 -0500

    Update BouncingBall.py

--- BouncingBall.py
+++ BouncingBall.py
@@ -2,10 +2,20 @@ from tkinter import *
 import random
 import time
 
+class Score:
+    def __init__(self, canvas, color):
+        self.score = 0
+        self.canvas = canvas
+        self.id = canvas.create_text(550, 10, text=self.score, fill=color)
+    def hit(self):
+        self.score += 10
+        self.canvas.itemconfig(self.id, text=self.score)
+
 class Ball:
-    def __init__(self, canvas, paddle, color):
+    def __init__(self, canvas, paddle,score, color):
         self.canvas = canvas
         self.paddle = paddle
+        self.score = score
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
         self.canvas.move(self.id, 240, 100)
         starts = [-3, -2, -1, 1, 2, 3]
@@ -16,7 +26,7 @@ class Ball:
         #returns current height of canvas
         self.canvas_height = self.canvas.winfo_height()
         self.canvas_width = self.canvas.winfo_width()
-        # stop when het the bottom
+        # stop when hit the bottom
         self.hit_bottom = False
 
     def hit_paddle(self, pos):
@@ -25,6 +35,7 @@ class Ball:
         if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
             if pos[3] >= paddle_pos[1] and pos[3] <=  paddle_pos[3]:
                 self.x += self.paddle.x
+                self.score.hit()
                 return True
         return False
 
@@ -32,21 +43,21 @@ class Ball:
         self.canvas.move(self.id, self.x, self.y)
         pos = self.canvas.coords(self.id)
         if pos[1] <= 0:
-            self.y = 2
+            self.y = 1.5
         if pos[3] >= self.canvas_height:
             self.hit_bottom = True
         if self.hit_paddle(pos) == True:
-            self.y = -2
+            self.y = -1.5
         if pos[0] <= 0:
-            self.x = 2
+            self.x = 1.5
         if pos[2] >= self.canvas_width:
-            self.x = -2
+            self.x = -1.5
 
 class Paddle:
     def __init__(self, canvas, color):
         self.canvas = canvas
         self.id = canvas.create_rectangle(0, 0, 100, 10, fill=color)
-        self.canvas.move(self.id, 200, 300)
+        self.canvas.move(self.id, 200, 400)
         # starting position of the paddle
         self.x = 0
         self.canvas_width = self.canvas.winfo_width()
@@ -81,7 +92,9 @@ canvas.pack()
 tk.update()
 
 paddle = Paddle(canvas, 'brown')
-ball = Ball(canvas, paddle, 'red')
+score = Score(canvas, 'blue')
+ball = Ball(canvas, paddle, score,'red')
+
 game_over = canvas.create_text(300, 300, text="Game Over",font="bold", state="hidden")
 
 while True:
@@ -89,7 +102,7 @@ while True:
         ball.draw()
         paddle.draw()
     if ball.hit_bottom == True:
-        time.sleep(0.5)
+        time.sleep(0.7)
         canvas.itemconfig(game_over, state='normal')
     tk.update_idletasks()
     tk.update()
